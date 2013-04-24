98951c15b53441b0147d305075e319c4552b2686
Dave White <dwhite@yieldbot.com>
2013-04-24 14:19:11 -0400

    fix graphite apache_site recipe

--- cookbooks/graphite/recipes/apache_site.rb
+++ cookbooks/graphite/recipes/apache_site.rb
@@ -2,8 +2,8 @@ include_recipe 'apache2::mod_python'
 
 template "#{node[:apache][:dir]}/sites-available/graphite.conf" do
   mode          0644
-  variables     :web_dir => dashboard_dir,
-                :log_dir => node[:graphite][:log_dir]
+  variables     :web_dir => node[:graphite][:home_dir],
+                :log_dir => node[:graphite][:dashboard][:log_dir]
   source        "graphite-vhost.conf.erb"
 end
 
