744da84b109928d348a816327d163a2aca288bea
Dave White <dwhite@yieldbot.com>
2013-05-08 02:54:55 -0400

    change how gmond python module template source is found

--- cookbooks/ganglia/definitions/python_module.rb
+++ cookbooks/ganglia/definitions/python_module.rb
@@ -9,7 +9,7 @@ define :gmond_python_module, :module_type => nil, :template_cookbook => "ganglia
   python_modules_dir = "#{node['ganglia']['lib_dir']}/python_modules"
 
   template "#{confd_dir}/#{module_name}.pyconf" do
-    source "#{module_type}.pyconf.erb"
+    source "gmond_#{module_type}.pyconf.erb"
     cookbook params[:template_cookbook]
     owner "ganglia"
     group "ganglia"
@@ -19,7 +19,7 @@ define :gmond_python_module, :module_type => nil, :template_cookbook => "ganglia
   end
   
   template "#{python_modules_dir}/#{module_name}.py" do
-    source "#{module_type}.py.erb"
+    source "gmond_#{module_type}.py.erb"
     cookbook params[:template_cookbook]
     owner "ganglia"
     group "ganglia"
